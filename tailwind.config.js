194eb021a291a41b6521121d6799366e3a32ad69
Shaan Sundar <shaan.idtindia@gmail.com>
2022-04-12 23:53:11 +0530

    v1.1.3 - Font update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     extend: {
       colors:{
         'Gold': '#EDB70F',
+        'Gradient': 'linear-gradient(180deg, rgba(109,255,255,1) 0%, rgba(42,120,251,1) 100%)',
+        'Light': '#6dffff',
+        'Dark': '#2a78fb',
+      },
+      fonts:{
+        'ThunderboldDemo':'ThunderboldDemo',
+        'Poppins':'Poppins',
       }
     },
   },
