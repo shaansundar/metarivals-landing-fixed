37b2ab1ef8484300969391995a2bd58417d295a3
Shaan Sundar <shaan.idtindia@gmail.com>
2022-04-19 18:41:08 +0530

    v1.3.2 - Faster Loading & empty recursion

--- src/app/pages/nftmarketplace/nftmarketplace.component.ts
+++ src/app/pages/nftmarketplace/nftmarketplace.component.ts
@@ -11,7 +11,7 @@ export class NftmarketplaceComponent implements OnInit {
   constructor() { }
   public list: number[] = [];
   ngOnInit(): void {
-    for(let i=1; i<9; i++){
+    for(let i=1; i<7; i++){
       this.list.push(i);
     }
   }
